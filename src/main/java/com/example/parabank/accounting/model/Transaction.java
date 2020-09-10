494de7f9ab360320546c5e52f2deed8a4b5d66a1
piyushbhatia891 <piyushbhatia89@gmail.com>
2020-09-10 17:12:21 +0530

    Update Transaction.java

--- src/main/java/com/example/parabank/accounting/model/Transaction.java
+++ src/main/java/com/example/parabank/accounting/model/Transaction.java
@@ -1,14 +1,36 @@
 package com.example.parabank.accounting.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Transaction {
-	
-	private Integer balance;
-	private String bankAccountType;
-	
 
+	private Integer id;
+	private Integer accountId;
+	private double amount;
+	private String type;
+	
+	
+	public Integer getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(Integer accountId) {
+		this.accountId = accountId;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 }
